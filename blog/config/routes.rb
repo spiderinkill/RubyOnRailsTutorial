95dfbe2457e2be23811cc6b8719ff4956047dfd3
Zih Siang Syu <zihsiangsyu@Siangs-Mac.local>
2020-08-14 11:03:06 -0700

    1) Create resource: articles 2) Create Article controllers 3) add new.html.erb 4) add new and create in ArticleController

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # mapping url /joyce to the controller "welcome"'s method "index"
+  get 'joyce', to: 'welcome#index'
   get 'welcome/index'
+
+  #standard REST resource
+  resources :articles
+
   # let root to be "controller welcome" with "method index"
   root 'welcome#index'
 end
